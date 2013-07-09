4ff3c3b0fd9505fb5b36545b7719c277b5c4a32c
IvanTorre <ivan.torre@gmx.com>
2013-07-09 19:57:51 +0200

    Riaggiunta una classe ingiustamente rimossa.

---
+++ AgenziaDiViaggio/src/gestione_Catalogo/RemoFaRimaConScemo.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package gestione_Catalogo;
+
+/**
+ * @authors
+ * Remo Sperlongano
+ * Ivan Torre
+ */
+public class RemoFaRimaConScemo {
+	
+	private String remo;
+	
+	public RemoFaRimaConScemo() {
+		remo = "scemo";
+	}
+
+}
